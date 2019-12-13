53badf9f5a04da048621244f04e53e8e66de83ea
Noah <noahcoder77@gmail.com>
2019-12-13 17:01:59 -0500

    Create CoffeeTest.java

---
+++ test/CoffeeTest.java
@@ -0,0 +1,89 @@
+import coffeedraw.*;
+import java.awt.*;
+import javax.swift.*;
+import java.util.*;
+
+public class CoffeeTest extends WindowController {
+    
+    public static void main(String[] args) {
+        new CoffeeTest().startController(800, 600, "Coffee Test");
+    }
+    
+    public void begin() {
+        
+    }
+    
+    public void onMousePress(Location point) {
+        
+    }
+    
+    public void onMouseRelease(Location point) {
+    
+    }
+    
+    public void onMouseMove(Location point) {
+    
+    }
+    
+    public void onMouseDrag(Location point) {
+    
+    }
+    
+    public void onMouseEnter(Location point) {
+    
+    }
+    
+    public void onMouseExit(Location point) {
+    
+    }
+    
+    public void onUpPress() {
+    
+    }
+    
+    public void onUpPress() {
+    
+    }
+    
+    public void onUpRelease() {
+    
+    }
+    
+    public void onDownPress() {
+    
+    }
+    
+    public void onDownRelease() {
+    
+    }
+    
+    public void onLeftPress() {
+    
+    }
+    
+    public void onLeftRelease() {
+    
+    }
+    
+    public void onRightPress() {
+    
+    }
+    
+    public void onRightRelease() {
+    
+    }
+    
+    public void onBackspaceHeld() {
+    
+    }
+    
+    public static class Player extends ActiveObject {
+        private FilledRect body;
+        private Collection<FilledOval> wheels;
+        
+        
+        
+        
+    }
+    
+}
