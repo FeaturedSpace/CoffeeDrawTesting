e4280ee0d60a0851a2cf3d414e93e2987ee592dc
Noah <noahcoder77@gmail.com>
2019-12-13 12:59:05 -0500

    Update and rename WindowTest.java to WindowControllerTest.java

--- test/WindowControllerTest.java
+++ test/WindowControllerTest.java
@@ -5,17 +5,17 @@ import java.awt.*;
  * This test is designed to test the various constructors of the WindowController (A Controller that has a canvas and helper
  * methods. It also may use arguments to test the 3 different intializer methods of the Controller class.
  */
-public class WindowTest extends WindowController {
+public class WindowControllerTest extends WindowController {
     
     public static void main(String[] args) {
         if(args == null || args == new String[0]) 
-            new WindowTest.startController();
+            new WindowControllerTest.startController();
         
         if(args[0] == "size")
-            new WindowTest.startController(800, 600);
+            new WindowControllerTest.startController(800, 600);
             
         if(args[0] == "name")
-            new WindowTest.startController(800, 600, "Custom_Name");
+            new WindowControllerTest.startController(800, 600, "Custom_Name");
         
     }
     
